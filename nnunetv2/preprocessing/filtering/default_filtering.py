9cd936a627dae6838b03474e466fa5039f15c02f
Quantico-Bullet <Brian.Kagiso.Lebang@gmail.com>
2025-06-05 15:50:54 +0200

    CLAHE #2

--- nnunetv2/preprocessing/filtering/default_filtering.py
+++ nnunetv2/preprocessing/filtering/default_filtering.py
@@ -1,9 +1,14 @@
 from skimage.exposure import equalize_adapthist
+import numpy as np
 
-def apply_clahe(data):
+def apply_clahe(data: np.ndarray):
     """
     """
-    for c in range(1, data.shape[0]):
+    data = data.astype(np.float32, copy=False)
+    
+    for c in range(data.shape[0]):
+        data[c]-= data[c].min()
+        data[c] /= np.clip(data[c].max(), a_min=1e-8, a_max=None)
         data[c] = equalize_adapthist(data[c], clip_limit = 0.2)
 
-    return data
+    return data.astype(np.float32)
